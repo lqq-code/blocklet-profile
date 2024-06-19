dbf9edd6944db9a995e5bd7b4d179f512abc96cf
lqq-code <15659622723@163.com>
2024-06-19 16:57:11 +1000

    feat: add unit tests for TypographyInfo component

--- src/component/TypographyInfo/TypographyInfo.jsx
+++ src/component/TypographyInfo/TypographyInfo.jsx
@@ -5,7 +5,7 @@ import Skeleton from '@mui/material/Skeleton';
 const TypographyInfo = ({ label, value, loading }) => (
   <Typography sx={{ mb: 2 }} variant="body2" color="text.secondary" align="left">
     <span style={{ marginRight: 20 }}>{label}:</span>
-    {loading ? <Skeleton /> : value}
+    {loading ? <Skeleton data-testid="skeleton" /> : value}
   </Typography>
 );
 
---
+++ src/component/TypographyInfo/TypographyInfo.test.jsx
@@ -0,0 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import TypographyInfo from './TypographyInfo';
+
+describe('TypographyInfo Component', () => {
+  it('renders label and value when loading is false', () => {
+    const label = 'Name';
+    const value = 'John Doe';
+    const loading = false;
+
+    const { getByText, queryByTestId } = render(
+      <TypographyInfo label={label} value={value} loading={loading} />
+    );
+
+    expect(getByText(/Name:/i)).toBeInTheDocument();
+    expect(queryByTestId('skeleton')).toBeNull(); 
+    expect(getByText('John Doe')).toBeInTheDocument(); 
+  });
+  it('renders Skeleton when loading is true', () => {
+    const label = 'Name';
+    const loading = true;
+
+    const { getByTestId } = render(
+      <TypographyInfo label={label} loading={loading} />
+    );
+
+    const skeleton = getByTestId('skeleton');
+    expect(skeleton).toBeInTheDocument(); // Check if Skeleton (loading indicator) is rendered
+  });
+});
